b0aabb9a4a806891cf90ad7521296cf7dca4adf3
Geeta Khanchi <geetakhanchi@Geetas-MacBook-Pro.local>
2023-08-23 17:10:22 +0530

    updates

--- src/components/allusers.js
+++ src/components/allusers.js
@@ -1,9 +1,40 @@
+import { Table, TableHead, TableRow, TableCell, TableBody } from "@mui/material"
+import { getAllUsers } from "../service/api"
+import { useState, useEffect } from "react"
 
 const AllUsers = () => {
+    const [users, setUsers] = useState([])
+    
+        useEffect(() => {
+            getAllUsers().then(res => {
+                setUsers(res.data)
+            })
+        }, [])
     return (
-        <div>
-            <h1>All Users</h1>
-        </div>
+        <Table>
+            <TableHead>
+                <TableRow>
+                    <TableCell>ID</TableCell>
+                    <TableCell>Name</TableCell>
+                    <TableCell>Username</TableCell>
+                    <TableCell>Email</TableCell>
+                    <TableCell>Phone</TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {
+                    users.map(user => (
+                        <TableRow>
+                            <TableCell>{user.id}</TableCell>
+                            <TableCell>{user.name}</TableCell>
+                            <TableCell>{user.username}</TableCell>
+                            <TableCell>{user.email}</TableCell>
+                            <TableCell>{user.phone}</TableCell>
+                        </TableRow>    
+                    ))        
+                }
+            </TableBody>
+        </Table>
     )
 }
 
--- src/service/api.js
+++ src/service/api.js
@@ -1,13 +1,22 @@
 import axios from "axios";
 
 
-const API_url = " "
+const API_url = "http://127.0.0.1:3002/users";
 
 export  const addUser = async(data) => {
     try {
     return await axios.post(API_url, data);
     } catch (error) {
         console.log('Error while calling Adduser api',error.message);
+    }
+
+};
+
+export  const getAllUsers = async() => {
+    try {
+    return await axios.get(API_url);
+    } catch (error) {
+        console.log('Error while calling GetAllUsers api',error.message);
     }
 
 };
